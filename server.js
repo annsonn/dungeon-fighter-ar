acc97976707236303571b745f5c3d96293c8c276
Glitch (plume-budget) <none>
2017-11-01 19:05:06 +0000

    👹🏏 Updated with Glitch

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 //app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
